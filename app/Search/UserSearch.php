abc63c5c745e5a8cf4c014f12d0778045976d443
Sergio Ojeda <soj3da@gmail.com>
2018-03-22 17:41:17 -0300

    Refact

--- app/Search/UserSearch.php
+++ app/Search/UserSearch.php
@@ -3,35 +3,40 @@
 namespace App\Search;
 
 use App\User;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
 
 class UserSearch
 {
     public static function apply(Request $filters)
     {
-        $query = (new User)->newQuery();
-
-        $query = static::applyFiltersToQuery($filters, $query);
-
-        return $query->get();
+        $query = static::applyDecoratorsFromRequest($filters, (new User)->newQuery());
+        return static::getResults($query);
     }
 
-    private static function applyFiltersToQuery(Request $filters, Builder $query)
+    private static function applyDecoratorsFromRequest(Request $request, Builder $query)
     {
-        foreach ($filters->all() as $filterName => $value) {
-
-            $decorator =
-                __NAMESPACE__ . '\\Filters\\' .
-                str_replace(' ', '', ucwords(
-                    str_replace('_', ' ', $filterName)));
-
-            if (class_exists($decorator)) {
+        foreach ($request->all() as $filterName => $value) {
+            $decorator = static::createFilterDecorator($filterName);
+            if (static::isValidDecorator($decorator)) {
                 $query = $decorator::apply($query, $value);
             }
-
         }
-
         return $query;
+    }
+
+    private static function createFilterDecorator($name)
+    {
+        return __NAMESPACE__ . '\\Filters\\' . studly_case($name);
+    }
+
+    private static function isValidDecorator($decorator)
+    {
+        return class_exists($decorator);
+    }
+
+    private static function getResults(Builder $query)
+    {
+        return $query->get();
     }
 }
